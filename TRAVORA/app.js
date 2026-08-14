20530cc6536c1e98ca61f89fd49fb35d59d0ae92
Amma Anjali <ammaanjali578@gmail.com>
2026-08-14 23:12:19 +0530

    Test Atlas listings

--- TRAVORA/app.js
+++ TRAVORA/app.js
@@ -89,6 +89,14 @@ app.use("/", userRouter);
 app.get("/", (req, res) => {
     res.redirect("/listings");
 });
+
+app.get("/test-db", async (req, res) => {
+    const count = await mongoose.connection.db
+        .collection("listings")
+        .countDocuments();
+
+    res.send(`Listings in database: ${count}`);
+});
 // if path doesn't exists (placed at the end of all routes otherwise it will block valid routes.)
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
